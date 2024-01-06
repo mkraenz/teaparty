33527d11b203672e177b84b391f1748a7f3c4e20
Mirco Kraenz <software@kraenz.eu>
2024-01-06 10:13:08 +0100

    fix: error page doesnt need the nav

--- apps/admin-web/src/app/globals/components/ErrorPage.tsx
+++ apps/admin-web/src/app/globals/components/ErrorPage.tsx
@@ -1,14 +1,33 @@
-import { useAuth } from '@teaparty/react-firebase-auth';
-import { Layout } from '@teaparty/shared-ui';
-import ErrorDisplay from './ErrorDisplay';
+import { Button, Heading, Image, Text, VStack } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate, useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
-  const { authenticated } = useAuth();
-  if (!authenticated) return <ErrorDisplay />;
+  const nav = useNavigate();
+  const { t } = useTranslation();
+  const error = useRouteError() as Error & {
+    statusText?: string;
+    status?: number;
+  };
+
+  const isHttpErrorCode =
+    error.status && error.status > 400 && error.status < 600;
   return (
-    <Layout>
-      <ErrorDisplay />
-    </Layout>
+    <VStack h={'full'} justify="center">
+      <Heading>{t('Oops!')}</Heading>
+      <br />
+      <Text>{t('Sorry, an error occurred.')}</Text>
+      <Text as="i">{error.statusText || error.message}</Text>
+      <Button onClick={() => nav('/')}>{t('Back to safety')}</Button>
+      <br />
+      {isHttpErrorCode && (
+        <Image
+          src={`https://http.cat/images/${error.status}.jpg`}
+          alt={error.statusText}
+          maxH={'50%'}
+        />
+      )}
+    </VStack>
   );
 };
 
