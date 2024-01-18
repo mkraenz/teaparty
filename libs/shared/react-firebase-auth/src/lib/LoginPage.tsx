b7cd764d6ebb246c8764d691b6d2b02a6debf144
Mirco Kraenz <software@kraenz.eu>
2024-01-18 18:01:38 +0100

    fix: if authenticated on login page, redirect to home

--- libs/shared/react-firebase-auth/src/lib/LoginPage.tsx
+++ libs/shared/react-firebase-auth/src/lib/LoginPage.tsx
@@ -1,13 +1,21 @@
 import { Center, Grid, GridItem, Heading, VStack } from '@chakra-ui/react';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Navigate } from 'react-router-dom';
 import LoginAppbar from './LoginAppbar';
 import LoginForm from './LoginForm';
+import { useAuth } from './hooks';
 
 interface Props {}
 
 export const LoginPage: FC<Props> = () => {
+  const { authenticated } = useAuth();
   const { t } = useTranslation();
+
+  if (authenticated) {
+    return <Navigate to={'/'} replace />;
+  }
+
   return (
     <Grid
       templateAreas={{
