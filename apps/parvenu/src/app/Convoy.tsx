5e56a1752795745aa034a28f4834d6daf9d239c3
Mirco Kraenz <software@kraenz.eu>
2024-03-17 18:42:06 +0100

    feat(parvenu): improve convoy selection and double clicks

--- apps/parvenu/src/app/Convoy.tsx
+++ apps/parvenu/src/app/Convoy.tsx
@@ -1,7 +1,6 @@
 import { IconButton } from '@chakra-ui/react';
 import { FC, MouseEventHandler } from 'react';
 import { MdSailing } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
 import { useConvoy } from './GameProvider';
 import { useConvoySelector } from './SelectionProvider';
 
@@ -11,23 +10,15 @@ type Props = {
 
 const Convoy: FC<Props> = ({ id }) => {
   const convoy = useConvoy(id);
-  const nav = useNavigate();
   const selector = useConvoySelector();
   if (!convoy) return null;
   const handleSelect: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.stopPropagation();
-    if (selector.selected === convoy.id) {
-      return nav(`/convoys/${convoy.id}`);
-    } else {
-      selector.setSelected(convoy.id);
-    }
+    selector.setSelectedWithNavEffect(convoy.id);
   };
   return (
     <IconButton
       onClick={handleSelect}
-      onDoubleClick={() => convoy.setTarget({ x: 115, y: 700 })}
-      // as={Link}
-      // to={`/convoys/${convoy.id}`}
       icon={<MdSailing />}
       aria-label={convoy.label}
       pos={'absolute'}
--- apps/parvenu/src/app/SelectionProvider.tsx
+++ apps/parvenu/src/app/SelectionProvider.tsx
@@ -2,26 +2,46 @@ import {
   FC,
   PropsWithChildren,
   createContext,
+  useCallback,
   useContext,
   useState,
 } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SelectionContext = createContext<{
   /** id of the selected convoy */
   selected: string;
   setSelected: (id: string) => void;
+  setSelectedWithNavEffect: (id: string) => void;
 }>({
   selected: '',
   setSelected: () => {},
+  setSelectedWithNavEffect: () => {},
 });
 
 export const SelectionProvider: FC<
   PropsWithChildren<{ logicFps?: number }>
 > = ({ children }) => {
+  const nav = useNavigate();
   const [selected, setSelected] = useState('');
+  const [selectionTime, setSelectionTime] = useState(0);
+
+  const setSelectedWithNavEffect = useCallback(
+    (val: string) => {
+      setSelected(val);
+      const isDoubleClick = selectionTime > Date.now() - 300;
+      if (isDoubleClick) {
+        nav(`/convoys/${val}`);
+      }
+      setSelectionTime(Date.now());
+    },
+    [selectionTime]
+  );
 
   return (
-    <SelectionContext.Provider value={{ selected, setSelected }}>
+    <SelectionContext.Provider
+      value={{ selected, setSelected, setSelectedWithNavEffect }}
+    >
       {children}
     </SelectionContext.Provider>
   );
