83550b47ad1fbefde6578d4ab01fb239da4b1be9
Mirco Kraenz <software@kraenz.eu>
2024-03-19 08:24:20 +0100

    refactor(parvenu): prototype pathfinding

--- apps/parvenu/src/app/PathFindingTest.tsx
+++ apps/parvenu/src/app/PathFindingTest.tsx
@@ -1,93 +1,96 @@
 import { Circle, Square } from '@chakra-ui/react';
-import { NavMesh, buildPolysFromGridMap } from 'navmesh';
-import { FC } from 'react';
+import { NavMesh } from 'navmesh';
+import { FC, useMemo, useState } from 'react';
+import navmeshMatrix from './navmesh-matrix.json';
+import navmeshPolygons from './navmesh-polygons.json';
 
 type Props = {};
 
-const Rect: FC<{ walkable: boolean; tilesize: number; isOnPath?: boolean }> = ({
-  walkable,
-  tilesize,
-  isOnPath,
-}) => {
+const Rect: FC<{
+  walkable: boolean;
+  tilesize: number;
+  isOnPath?: boolean;
+  onClick: VoidFunction;
+  onContextMenu: VoidFunction;
+}> = ({ walkable, tilesize, isOnPath, onClick, onContextMenu }) => {
   return (
     <Square
-      border={'white'}
-      borderWidth={1}
       size={tilesize}
       borderStyle={'solid'}
-      backgroundColor={isOnPath ? 'green' : walkable ? undefined : 'red'}
+      backgroundColor={isOnPath ? 'green' : walkable ? 'blue' : 'black'}
+      onClick={onClick}
+      onContextMenu={onContextMenu}
     />
   );
 };
 
 const PathFindingTest: FC<Props> = (props) => {
   /** 1 = walkable */
-  const map = [
-    [1, 1, 1, 0, 1],
-    [0, 1, 1, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1],
-  ];
-  const tilesize = 30;
-  const polys = buildPolysFromGridMap(map, tilesize, tilesize); // the tile size is how small the smallest rectangle is.
-  const navMesh = new NavMesh(polys);
-  const from = { x: 0, y: 0 };
-  const to = { x: 4 * tilesize, y: 18 * tilesize };
+  const map = navmeshMatrix;
+  const tilesize = 1;
+  const [from, setFrom] = useState({ x: 20, y: 5 });
+  const [to, setTo] = useState({ x: 150 * tilesize, y: 110 * tilesize });
   console.log(to);
-  const path = navMesh.findPath(from, to);
-  const downscaledPath = path?.map((p) => ({
-    x: p.x / tilesize,
-    y: p.y / tilesize,
-  }));
-  console.log(path);
+  const path = useMemo(() => {
+    // const polys = buildPolysFromGridMap(map, tilesize, tilesize); // the tile size is how small the smallest rectangle is.
+    const polys = navmeshPolygons;
+    // TODO: export the polys to a file instead of recalculating them
+    const navMesh = new NavMesh(polys);
+    const path = navMesh.findPath(from, to);
+    console.log(
+      'path',
+      path?.map((p) => `${p.x} ${p.y}`)
+    );
+    return path;
+  }, [from, to]);
+
+  const downscaledPath = useMemo(
+    () =>
+      path?.map((p) => ({
+        x: p.x / tilesize,
+        y: p.y / tilesize,
+      })),
+    [path]
+  );
+
   return (
     <div>
       {map.map((row, y) => {
         return (
           <div key={y} style={{ display: 'flex' }}>
             {row.map((col, x) => {
+              if (y > 120 || x > 180) return null;
               return (
                 <Rect
                   key={x}
                   walkable={col === 1}
                   tilesize={tilesize}
                   isOnPath={downscaledPath?.some((p) => p.x === x && p.y === y)}
+                  onClick={() => {
+                    console.log('clicked', x, y);
+                    setTo({ x: x * tilesize, y: y * tilesize });
+                  }}
+                  onContextMenu={() => {
+                    console.log('right clicked', x, y);
+                    setFrom({ x: x * tilesize, y: y * tilesize });
+                  }}
                 />
               );
             })}
           </div>
         );
       })}
-      <Circle
-        position={'absolute'}
-        backgroundColor={'pink'}
-        top={from.y}
-        left={from.x}
-        size={3}
-      />
-      <Circle
-        position={'absolute'}
-        backgroundColor={'violet'}
-        top={to.y}
-        left={to.x}
-        size={3}
-      />
+      {path &&
+        path.map((p) => (
+          <Circle
+            position={'absolute'}
+            backgroundColor={'blue'}
+            top={p.y}
+            left={p.x}
+            size={3}
+            key={`${p.x}-${p.y}`}
+          />
+        ))}
     </div>
   );
 };
