fc9f703886c6731f457827c999b3e194f8873848
Mirco Kraenz <software@kraenz.eu>
2024-05-03 17:43:35 +0200

    feat(parvenu): hitting ESC inside a city returns to the worldmap

--- apps/parvenu/src/app/common/ToWorldmapButton.tsx
+++ apps/parvenu/src/app/common/ToWorldmapButton.tsx
@@ -2,8 +2,10 @@ import { IconButton } from '@chakra-ui/react';
 import { FC } from 'react';
 import { FiGlobe } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { useNavigateToHomeOnEscape } from '../hooks/useNavigateToHomeOnEscape';
 
 const ToWorldmapButton: FC = () => {
+  useNavigateToHomeOnEscape();
   return (
     <IconButton aria-label="Open map" icon={<FiGlobe />} as={Link} to={'/'} />
   );
